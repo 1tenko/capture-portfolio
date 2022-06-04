d8861f039318ce306d220ea3d2962e7d8decd24b
tenko <tajm.ogunbiyi@gmail.com>
2022-06-04 23:22:26 +0100

    :art: Global Styling done + started AboutSection styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+// Styles
+import GlobalStyle from "./components/styles/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,12 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styles
+import { About, Description } from "./styles/AboutSection.Styled";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
           <div className="hide">
             <h2>We work to make</h2>
@@ -23,9 +25,9 @@ const AboutSection = () => {
           </p>
           <button>Contact Us</button>
         </div>
-      </div>
+      </Description>
       <img src={home1} alt="guy with a camera" />
-    </div>
+    </About>
   );
 };
 
